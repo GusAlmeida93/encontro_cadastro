b641d85c6a26ba0d502faa690c11cc755b158d18
Gustavo Almeida <gusalmeida93@gmail.com>
2019-09-09 09:10:16 -0300

    ajustes

--- encontro_cadastro/settings.py
+++ encontro_cadastro/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = "encontro_cadastro.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "encontrodb",
-        "USER": "postgres",
-        "PASSWORD": "django1234",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -119,8 +115,3 @@ USE_TZ = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "encontro_cadastro/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
