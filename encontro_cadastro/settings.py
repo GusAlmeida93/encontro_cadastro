d5286dc7aeafff6d6de560d915bc7b808724ba91
Gustavo Almeida <gusalmeida93@gmail.com>
2019-09-09 09:52:21 -0300

    ajustes

--- encontro_cadastro/settings.py
+++ encontro_cadastro/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = "encontro_cadastro.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "encontrodb",
-        "USER": "postgres",
-        "PASSWORD": "django1234",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
